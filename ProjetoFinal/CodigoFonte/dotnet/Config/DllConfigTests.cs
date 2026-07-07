7f6a3054bd6286620e8a7043ccd516ef2cf19869
Matheus Cruz <matheusmcruz2004@gmail.com>
2026-07-07 13:30:57 -0300

    Ajustando o workflow
    Configurando ci.yml
    
    feat: Ajustando configurações do Workflow
    
    Ajustes para funcionar no linux para o Actions
    
    Mudança para o Actions do windows evitando conflitos
    
    Ajuste no workflow
    
    Mais ajustes no workflow
    
    Novo ajuste no workflow
    
    Outro ajuste no workflow
    
    Resolvendo compilação do COBOL no Actions
    
    Outro ajuste de dlls no workflow
    
    Mais ajustes no workflow
    
    Testando possivel solução
    
    Mais ajustes
    
    outro teste
    
    Mais um
    
    Novo ajuste
    
    Instalando outro GnuCOBOL
    
    mais mudanças
    
    Voltando o ci.yml
    
    Configurando o ci.yml
    
    Ajustando para integração correta
    
    Continuando configuração
    
    Ajustes
    
    Mudanças para MSYS2
    
    Alterando o arquivo .yml
    
    Outra tentativa
    
    Alteração no arquivo

--- ProjetoFinal/CodigoFonte/dotnet/Config/DllConfigTests.cs
+++ ProjetoFinal/CodigoFonte/dotnet/Config/DllConfigTests.cs
@@ -5,18 +5,18 @@ namespace dotnet.Config.DllConfigTests;
 public class DllConfigTests 
 {
     //impota a função de inicialização do runtime COBOL
-    [DllImport("libcob-4.dll", CallingConvention = CallingConvention.Cdecl)]
+    [DllImport("libcob-4", CallingConvention = CallingConvention.Cdecl)]
     public static extern void cob_init(int argc, IntPtr argv);
 
-    [DllImport(@"..\..\..\..\COBOL\SOURCE\CONSCLI.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(@"..\..\..\..\COBOL\SOURCE\CONSCLI", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
      public static extern void CONSCLI(byte[] argumento);
 
-    [DllImport(@"..\..\..\..\COBOL\SOURCE\CADASCLI.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(@"..\..\..\..\COBOL\SOURCE\CADASCLI", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
      public static extern void CADASCLI(byte[] argumento);
 
-    [DllImport(@"..\..\..\..\COBOL\SOURCE\ATUACLI.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(@"..\..\..\..\COBOL\SOURCE\ATUACLI", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
      public static extern void ATUACLI(byte[] argumento);
      
-    [DllImport(@"..\..\..\..\COBOL\SOURCE\DELECLI.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
+    [DllImport(@"..\..\..\..\COBOL\SOURCE\DELECLI", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Ansi)]
      public static extern void DELECLI(byte[] argumento);
 }     
